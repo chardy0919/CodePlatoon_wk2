d377b7c12c1d36364fd14bb8289dfc065cdcc8bc
Cody Hardy <chardy0919@gmail.com>
2023-10-07 15:53:54 -0400

    Cody, first commit

---
+++ Practice/intro_python_OOP.py
@@ -0,0 +1,12 @@
+# Five Pillars of OOP:
+
+# Encapsulation = Data access can be restricted based on needs
+# Abstraction = Logic/Complexity is hidden, for simplicity
+# Inheritance = Sharing of similar features and attributes (Is-a)
+# Composition = Including other Objects as attributes (Has-a)
+# Polymorphism = Objects can take different forms depending on needs/situation
+
+class Dog:
+    pass
+
+fido = Dog()
